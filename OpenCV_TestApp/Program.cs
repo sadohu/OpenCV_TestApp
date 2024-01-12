713c20f5d199dbe30370bea4abbeb75a0aa9d102
sadohu <sadohu26@yahoo.com>
2024-01-12 10:51:55 -0500

    Agregar archivos de proyecto.

---
+++ OpenCV_TestApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenCV_TestApp;
+
+Console.WriteLine("Hello, World!");
+
+VideoFramesConverter videoConverter = new VideoFramesConverter();
+Console.WriteLine("Ingrese la ruta del video de referencia");
+string route = Console.ReadLine();
+videoConverter.Converter(route);
+
+
---
+++ OpenCV_TestApp/VideoFramesConverter.cs
@@ -0,0 +1,42 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCV_TestApp
+{
+	internal class VideoFramesConverter
+	{
+		public void converter(String route)
+		{
+			Console.WriteLine("Ingrese la ruta del video:");
+			string route = Console.ReadLine();
+
+
+			using (var videoCapture = new VideoCapture(route))
+			{
+				if (!videoCapture.IsOpened())
+				{
+					Console.WriteLine("No se pudo abrir el video.");
+					return;
+				}
+
+				int frameCount = (int)videoCapture.Get(VideoCaptureProperties.FrameCount);
+
+				for (int i = 0; i < frameCount; i++)
+				{
+					Mat frame = new Mat();
+					videoCapture.Read(frame);
+
+					if (frame.Empty())
+						break;
+
+					// Guardar cada frame como una imagen
+					Cv2.ImWrite($"E:/Files/workspace/Digital Solutions 324/frames/frame_{i}.jpg", frame);
+				}
+			}
+		}
+	}
+}
